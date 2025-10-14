426e3755b4e18698f1149fb42502689c50f3cabc
sandeepkatta0024 <sandeepkatta0024@gmail.com>
2025-10-14 12:54:52 +1030

    Added messagetype and Readme

---
+++ src/main/java/council/MessageType.java
@@ -0,0 +1,12 @@
+package council;
+
+public enum MessageType {
+    PREPARE,        // fields: from, n
+    PROMISE,        // fields: from, n, acceptedN, acceptedV
+    REJECT,         // fields: from, higherN
+    ACCEPT_REQUEST, // fields: from, n, v
+    ACCEPTED,       // fields: from, n, v
+    DECIDE,         // fields: from, v
+    PING,           // optional: liveness
+    PONG            // optional
+}
